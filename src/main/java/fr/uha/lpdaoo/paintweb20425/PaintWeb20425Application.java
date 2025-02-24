c0f6fe06b34562816e0865d375b98811cee906a4
Florent.Bourgeois <florent.bourgeois@uha.fr>
2025-02-24 13:38:04 +0100

    project ready

---
+++ src/main/java/fr/uha/lpdaoo/paintweb20425/PaintWeb20425Application.java
@@ -0,0 +1,13 @@
+package fr.uha.lpdaoo.paintweb20425;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintWeb20425Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaintWeb20425Application.class, args);
+    }
+
+}
---
+++ src/test/java/fr/uha/lpdaoo/paintweb20425/PaintWeb20425ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.uha.lpdaoo.paintweb20425;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintWeb20425ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
